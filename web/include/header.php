72e3c3856bd1d31e2f905b4780d618e65784c8f7
slevy <slevy@lalasystems.com>
2014-05-08 13:51:08 -0700

    basic layouts

---
+++ web/include/header.php
@@ -0,0 +1,24 @@
+<meta name="viewport" content="width=device-width, user-scalable=no">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
+<style type="text/css">
+    .ui-block-a  {
+        text-align: center;
+    }
+
+    .ui-block-b  {
+        text-align: left;
+    }
+    .ui-block-c {
+        text-align: right;
+    }
+
+    label {
+        height: 100px;
+        width: 150px;
+    }
+</style>
+<?php
+
+?>
