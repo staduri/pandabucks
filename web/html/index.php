f72fcd0de3b7aeecff71304cadcffd38fd73be1c
slevy <slevy@lalasystems.com>
2014-05-08 11:42:08 -0700

    stub ui

--- web/html/index.php
+++ web/html/index.php
@@ -1,11 +1,56 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, user-scalable=no">
         <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
         <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
         <script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
+        <style type="text/css">
+            .ui-block-a  {
+                text-align: center;
+            }
+        </style>
     </head>
     <body>
-         jquerymobile test page
+        <div class="ui-grid-b">
+            <div class="ui-block-a">
+                <fieldset class="ui-grid-a">
+                    <div class="ui-block-a"><input type="submit" value="Picks" data-theme="b" /></div>
+                    <div class="ui-block-b"><input type="reset" value="Leaderboard" data-theme="a" /></div>
+                </fieldset>
+            </div>
+            <div class="ui-block-a">
+                <div style="text-align: center">
+                    <h2>Friday, June 13th</h2>
+                    <h3>Make your picks</h3>
+                </div>
+            </div>
+            <div class="ui-block-a">
+                <ul data-role="listview" > <!-- data-inset="true" -->
+                    <li>
+                        <a href="">
+                        <div class="ui-grid-c">
+                            <div class="ui-block-a">
+                                <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/28QhjxboVH/200px-Flag_of_Uruguay.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH7XPUzC5U
+A1Sw" />
+                            </div>
+                            <div class="ui-block-b">
+                                <span style="font-family: Arial; color: #CCCCCC; text-align: center;">
+                                <h3>9:00 AM</h3>
+                                <h3>PST</h3>
+                                </span>
+                            </div>
+                            <div class="ui-block-c">
+                                <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/JIN-22f5we/200px-Flag_of_the_Netherlands.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH
+7XPUzC5UA1Sw" />
+                            </div>
+                            <div class="ui-block-d">Your pick</div>
+                        </div>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
     </body>
 </html>
