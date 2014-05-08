e414380840c78aab05260684789795ab99bee9ad
Sid Taduri <siddharth.taduri@gmail.com>
2014-05-08 13:14:12 -0700

    * Adding admin page

--- web/html/admin.php
+++ web/html/admin.php
@@ -1,22 +1,3 @@
-<?php
-
-require_once '../include/UserTools.php';
-
-$team1 = "";
-$team2 = "";
-
-//check to see if they've submitted the login form
-if(isset($_POST['action'])) {
-    echo "<script type='text/javascript'>alert('$_POST');</script>";
-    $team1 = $_POST['team1-score'];
-    $team2 = $_POST['team2-score'];
-
-    $userTools = new UserTools();
-
-    unset($_POST['action']);
-}
-?>
-
 <html>
 <head>
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
@@ -24,6 +5,27 @@ if(isset($_POST['action'])) {
     <script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
 </head>
 <body>
+
+<?php
+require_once '../include/UserTools.php';
+
+if (isset($_POST['action'])) {
+    $team1 = $_POST['team1-score'];
+    $team2 = $_POST['team2-score'];
+    $selection = $_POST['game'];
+
+    $userTools = new UserTools();
+    $data = array(
+        "game_id" => $selection,
+        "score_team_a" => $team1,
+        "score_team_b" => $team2
+    );
+
+    $userTools->saveResult($data);
+}
+
+?>
+
 <?php
 if($error != "") {
     echo $error."<br/>";
@@ -34,18 +36,19 @@ if($error != "") {
     <div data-role="navbar" style="padding-top: 15px">
         <ul>
             <li><a href="#input-scores" data-ajax="false">Enter Scores</a></li>
+            <li><a href="#results" data-ajax="false">Results</a></li>
         </ul>
     </div>
     <div id="input-scores" class="ui-body-d ui-content">
         <form action="admin.php" method="post">
-            <input list="games">
+            <input name="game" list="games">
             <datalist id="games">
                 <?php
                 $userTools = new UserTools();
                 $games = $userTools->getAllGames();
                 foreach ($games as $game) {
-                    echo("<option value='".$game["game_id"].". ".$game["team1"]." vs ". $game["team2"]. " - ". $game["stage"]
-                        ."'>\n");
+                    echo("<option value='".$game["game_id"]."'>".$game["team1"]." vs ". $game["team2"]. " - ". $game["stage"]
+                        ."</option>\n");
                 }
 
                 ?>
@@ -58,6 +61,35 @@ if($error != "") {
 
             <input type="submit" value="Save" name="submit-results" />
         </form>
+    </div>
+    <div id="results" class="ui-body-d ui-content">
+        <table data-role="table" data-mode="reflow" class="ui-responsive table-stroke">
+            <thead>
+                <tr>
+                    <th data-priority="1">Team A</th>
+                    <th data-priority="2">Team B</th>
+                    <th data-priority="3">Stage</th>
+                    <th data-priority="4">GS</th>
+                    <th data-priority="5">GS</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $userTools = new UserTools();
+                    $games = $userTools->getAllResults();
+                    foreach ($games as $game) {
+                        echo("<tr>"
+                            ."<td>".$game['team1_name']."</td>"
+                            ."<td>".$game['team2_name']."</td>"
+                            ."<td>".$game['stage']."</td>"
+                            ."<td>".$game['score_a']."</td>"
+                            ."<td>".$game['score_b']."</td>"
+                            ."</tr>\n");
+                    }
+
+                ?>
+            </tbody>
+        </table>
     </div>
 </div>
 </body>
