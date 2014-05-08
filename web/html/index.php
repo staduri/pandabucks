ae9bbff40948e664abf3a6ac956b007d4c0d1718
slevy <slevy@lalasystems.com>
2014-05-08 13:54:47 -0700

    basic layouts

--- web/html/index.php
+++ web/html/index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
+<?php
+
+require_once('../include/UserTools.php');
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+$userTools = new UserTools();
+
+$flags = $userTools->getAllFlags();
+$games = $userTools->getAllGames();
+
+?>
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, user-scalable=no">
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
-        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-        <script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
-        <style type="text/css">
-            .ui-block-a  {
-                text-align: center;
-            }
-        </style>
+        <?php require_once('../include/header.php') ?>
     </head>
     <body>
         <div class="ui-grid-b">
@@ -27,6 +32,7 @@
             </div>
             <div class="ui-block-a">
                 <ul data-role="listview" > <!-- data-inset="true" -->
+                    <?php foreach ($games as $game) { ?>
                     <li>
                         <a href="">
                         <div class="ui-grid-c">
@@ -48,91 +54,7 @@ A1Sw" />
                         </div>
                         </a>
                     </li>
-                    <li>
-                        <a href="">
-                            <div class="ui-grid-c">
-                                <div class="ui-block-a">
-                                    <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/28QhjxboVH/200px-Flag_of_Uruguay.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH7XPUzC5U
-A1Sw" />
-                                </div>
-                                <div class="ui-block-b">
-                                <span style="font-family: Arial; color: #CCCCCC; text-align: center;">
-                                <h3>9:00 AM</h3>
-                                <h3>PST</h3>
-                                </span>
-                                </div>
-                                <div class="ui-block-c">
-                                    <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/JIN-22f5we/200px-Flag_of_the_Netherlands.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH
-7XPUzC5UA1Sw" />
-                                </div>
-                                <div class="ui-block-d">Your pick</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <div class="ui-grid-c">
-                                <div class="ui-block-a">
-                                    <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/28QhjxboVH/200px-Flag_of_Uruguay.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH7XPUzC5U
-A1Sw" />
-                                </div>
-                                <div class="ui-block-b">
-                                <span style="font-family: Arial; color: #CCCCCC; text-align: center;">
-                                <h3>9:00 AM</h3>
-                                <h3>PST</h3>
-                                </span>
-                                </div>
-                                <div class="ui-block-c">
-                                    <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/JIN-22f5we/200px-Flag_of_the_Netherlands.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH
-7XPUzC5UA1Sw" />
-                                </div>
-                                <div class="ui-block-d">Your pick</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <div class="ui-grid-c">
-                                <div class="ui-block-a">
-                                    <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/28QhjxboVH/200px-Flag_of_Uruguay.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH7XPUzC5U
-A1Sw" />
-                                </div>
-                                <div class="ui-block-b">
-                                <span style="font-family: Arial; color: #CCCCCC; text-align: center;">
-                                <h3>9:00 AM</h3>
-                                <h3>PST</h3>
-                                </span>
-                                </div>
-                                <div class="ui-block-c">
-                                    <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/JIN-22f5we/200px-Flag_of_the_Netherlands.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH
-7XPUzC5UA1Sw" />
-                                </div>
-                                <div class="ui-block-d">Your pick</div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <div class="ui-grid-c">
-                                <div class="ui-block-a">
-                                    <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/28QhjxboVH/200px-Flag_of_Uruguay.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH7XPUzC5U
-A1Sw" />
-                                </div>
-                                <div class="ui-block-b">
-                                <span style="font-family: Arial; color: #CCCCCC; text-align: center;">
-                                <h3>9:00 AM</h3>
-                                <h3>PST</h3>
-                                </span>
-                                </div>
-                                <div class="ui-block-c">
-                                    <img width="100" height="66" src="https://dl.dropboxusercontent.com/sh/uzii4bdixy0gvqy/JIN-22f5we/200px-Flag_of_the_Netherlands.svg.png?token_hash=AAEM1ThQOdHafAvcUmAs620V7MXebXOrgH
-7XPUzC5UA1Sw" />
-                                </div>
-                                <div class="ui-block-d">Your pick</div>
-                            </div>
-                        </a>
-                    </li>
-
+                    <?php } ?>
                 </ul>
             </div>
         </div>
