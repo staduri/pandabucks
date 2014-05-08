ae5c8d2aa7e57d1511daaa638dba18040d914b86
Seth Levy <slevy@slevy-m02.ad.savagebeast.com>
2014-05-07 17:04:34 -0700

    jqm template

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
+        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
+    </head>
+    <body>
+         jquerymobile test page
+    </body>
+</html>
