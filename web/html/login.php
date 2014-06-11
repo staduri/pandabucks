5e88f2c8c1c0412201528a98f764b01ec383877b
Sid Taduri <siddharth.taduri@gmail.com>
2014-06-11 08:28:03 -0700

    Adding google analytics

--- web/html/login.php
+++ web/html/login.php
@@ -121,6 +121,15 @@ function js_redirect($url) {
             </div>
             <footer style="color: white; text-align: center">&copy; 2014 Pandathletico Soccer PERG</footer>
         </div>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+    ga('create', 'UA-51832818-1', '54.243.222.155');
+    ga('send', 'pageview');
+
+</script>
     </body>
 </html>
--- web/html/logout.php
+++ web/html/logout.php
@@ -18,5 +18,15 @@ if(isset($_SESSION) && array_key_exists("logged_in", $_SESSION) && $_SESSION["lo
 <div>
     <p>You will lose 30 points for doing this.</p>
 </div>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-51832818-1', '54.243.222.155');
+    ga('send', 'pageview');
+
+</script>
 </body>
 </html>
