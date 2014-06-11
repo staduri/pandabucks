63b56392915cfbe53a5fd40d2cc44c37aa72d61e
Sid Taduri <siddharth.taduri@gmail.com>
2014-06-11 08:54:23 -0700

    css changes make it more mobile friendly

--- web/html/index.php
+++ web/html/index.php
@@ -49,7 +49,7 @@ date_default_timezone_set('America/Los_Angeles');
                         <li>
                             <a href="pick.php?game=<?php echo $game["game_id"]; ?>">
                             <div class="col-md-8 col-md-offset-2">
-                                <div class="col-md-5" style="text-align: center;">
+                                <div class="col-md-4" style="text-align: center;">
                                     <img style="max-width: 100px; display: block; margin: auto;" width="90%" height="60" src="<?php echo $flags[$game["team1_id"]]; ?>" /><br/>
                                     <span><?php echo $game["team1"]; ?></span>
                                 </div>
@@ -62,7 +62,7 @@ date_default_timezone_set('America/Los_Angeles');
                                         <?php echo date("H:i", $game["time"]) ?> PST
                                     </span>
                                 </div>
-                                <div class="col-md-5" style="text-align: center;">
+                                <div class="col-md-4" style="text-align: center;">
                                     <img style="max-width: 100px;display: block; margin: auto;" width="90%" height="60" src="<?php echo $flags[$game["team2_id"]]; ?>" /><br/>
                                     <span><?php echo $game["team2"]; ?></span>
                                 </div>
