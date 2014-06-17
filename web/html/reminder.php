96160965bc78f2aca912e3c0e4daed3599d79221
Sid Taduri <siddharth.taduri@gmail.com>
2014-06-16 23:44:17 -0700

    sendgrid integ

--- web/html/reminder.php
+++ web/html/reminder.php
@@ -19,8 +19,40 @@ $headers .= "Reply-To: sid@pandacup.us" . "\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-$message = file_get_contents("reminder.html");
-echo $message;
-mail($to, $subject, $message, $headers);
+$html = file_get_contents("reminder.html");
+
+$url = 'https://api.sendgrid.com/';
+$user = 'sid@pandacup.us';
+$pass = 'PandaCup3242!';
+$to = 'sid@pandora.com';
+$params = array(
+    'api_user'  => $user,
+    'api_key'   => $pass,
+    'to'        => $to,
+    'subject'   => 'Reminder: Select your picks!',
+    'html'      => $html,
+    'text'      => '',
+    'from'      => 'sid@pandacup.us',
+);
+
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+print_r($response);
 
 ?>
